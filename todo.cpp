019865175c8995542b019b91e5b9b20bf97bf279
Chris Keith <chris.keith@oit.edu>
2022-10-09 20:33:01 -0700

    Created main menu

--- todo.cpp
+++ todo.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
 int main() {
-  std::cout << "todo: make a todo app..." << '\n';
+    bool mainmenu() {
+	char menu;	
+	cout << "\n Welcome to your ToDo list!";
+	cout << "\n\n\n Please choose from one of the following menu items\n";
+	cout << " (E)nter new Todo Task\n";
+	cout << " (V)iew your Todo List\n";
+	cout << " (Q)uit\n\n";
+	cout << " -> ";
+	std::cin >> menu;
+	cout << "\n";
+	switch (menu) {
+	case 'e':
+	case 'E':
+		//stub call to new task
+		system("pause");
+		system("cls");
+		return true;
+		break;
+	case 'v':
+	case 'V':
+		//call to view the list
+		system("pause");
+		system("cls");
+		return true;
+		break;
+	case 'q':
+	case 'Q':
+		return false;
+		break;
+	default:
+		cout << " Menu item not understood, please try again.\n\n";
+		system("pause");
+		system("cls");
+		return true;
+		break;
+		}
+	}
 }
