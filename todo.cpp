42aaeda73458da3c2eec4c37c3a4afe126e33e33
Chris Keith <chris.keith@oit.edu>
2022-10-10 18:49:21 -0700

    Created save to file, modified menu. Full IO now

--- todo.cpp
+++ todo.cpp
@@ -6,36 +6,40 @@
 
 using namespace std;
 std::ifstream inFILE;
+std::ofstream outFILE;
 std::vector<std::pair<int,string>> listOfPairs;
 
-bool mainmenu();
+bool mainmenu(char** argv);
 void openFile(std::ifstream& inFILE, char** argv);
 void parseList(std::ifstream& inFILE);
 void waitThenClear();
+void addListPair();
+void saveListOfPairs(std::ofstream& outFILE, char** argv);
 
 int main(int argc, char** argv) {
-	system("clear");	
+	//system("clear");	
 	openFile(inFILE, argv);
 	parseList(inFILE);
 	inFILE.close();
-	while(mainmenu());
+	
+	
+	while(mainmenu(argv));
 }
 
-bool mainmenu() {
+bool mainmenu(char** argv) {
 	char menu;	
 	cout << "\n Welcome to your ToDo list!";
 	cout << "\n\n\n Please choose from one of the following menu items\n";
 	cout << " (E)nter new Todo Task\n";
 	cout << " (V)iew your Todo List\n";
-	cout << " (Q)uit\n\n";
+	cout << " (S)ave and Quit\n\n";
 	cout << " -> ";
 	std::cin >> menu;
 	cout << "\n";
 	switch (menu) {
 	case 'e':
 	case 'E':
-		//create new vector item
-		waitThenClear();
+		addListPair();
 		return true;
 		break;
 	case 'v':
@@ -43,16 +47,19 @@ bool mainmenu() {
 		// display vector list	
 		// option to edit -> complete/uncomplete
 		// save vector to file
-		waitThenClear();
+		//waitThenClear();
+		saveListOfPairs(outFILE, argv);
 		return true;
 		break;
-	case 'q':
-	case 'Q':
+	case 's':
+	case 'S':
+		saveListOfPairs(outFILE, argv);
+		cout << "\nThank you, your file has been saved!\n";
 		return false;
 		break;
 	default:
 		cout << " Menu item not understood, please try again.\n\n";
-		waitThenClear();
+		//waitThenClear();
 		return true;
 		break;
 		}
@@ -64,13 +71,13 @@ void openFile(std::ifstream& inFILE, char** argv) {
 	if (!inFILE.is_open())	{
 		cout << " Sorry, there was an error opening your file! Please try again."
 			<< "\n\n";
-		waitThenClear();
+		//waitThenClear();
 	}	
-	waitThenClear();
+	//waitThenClear();
 }
 
 void waitThenClear(){
-	cin.clear();
+	cin.ignore();
 	cin.get();
 	system("clear");
 }
@@ -86,6 +93,22 @@ void parseList(std::ifstream& inFILE) {
 	}
 }
 
+void addListPair(){
+	string task;
+	cout << "\n\n Please enter your task: \n";		
+	cin.ignore();
+	std::getline(cin, task);
+	listOfPairs.emplace_back(0, task);
+	cout << "\nYour task kas been entered, thank you!\n";
+	//waitThenClear();
+}
+
+void saveListOfPairs(std::ofstream& outFILE, char** argv){
+	outFILE.open(argv[1]);
+	for(int i=0; i<listOfPairs.size();i++){
+		outFILE<<listOfPairs.at(i).first<<'|'<<listOfPairs.at(i).second <<'\n';
+	}	
+}
 // create save function
 // create delete function
 // edit completeness
