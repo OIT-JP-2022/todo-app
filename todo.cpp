fa576afc36b9b556b4bc079ca6672e9211b40544
Keegan Hauser <keeghauser08@gmail.com>
2022-10-10 18:38:52 -0700

    Added initial function definitions

--- todo.cpp
+++ todo.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+// Contributors: Nathan Green, Nate Castle, Keegan Hauser
 
-int main() {
-  std::cout << "todo: make a todo app..." << '\n';
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+using List = std::vector<std::pair<std::string, bool>>;
+
+std::ifstream LoadFile(std::string filename);
+std::string SaveFile(std::string filename);
+const void PrintList(List list);
+void SortList(List &list);
+void ToggleItem(std::string task);
+void AddItem(List &list);
+void DeleteItem(List &list);
+
+
+int main(int argc, char *argv[]) {
+
+    if(argc != 2)
+        return;
+
+    
 }
