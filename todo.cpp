8c7d4c4f36d9371ae1bc34aac57c230bddc0702b
Aaron Whitley <aaronwhitley84@DESKTOP-VP352K5.localdomain>
2022-10-11 13:05:21 -0700

    Just need to fill in functions for editing list

--- todo.cpp
+++ todo.cpp
@@ -46,16 +46,30 @@ Notes:
 #include <filesystem>
 #include <vector>
 #include <fstream>
+#include <iterator>
 
 using std::string;
 using std::cout;
 
+// Main menu function
+std::vector<string> MainMenu(std::vector<string> vect);  
 
+// Add item function
+std::vector<string> AddItem(std::vector<string> vect);
+
+// Delete item function
+std::vector<string> DeleteItem(std::vector<string> vect);
+
+// Toggle item done or not function
+std::vector<string> ToggleItemStatus(std::vector<string> vect);
+
+// Sort item list by completeness function
+std::vector<string> SortList(std::vector<string> vect);
+
+// Exit function
+std::vector<string> Exit(std::vector<string> vect);
 
 int main(int argc, char** argv) {
-
-//Get list from file or let user start to make a list.
-
 	// Get save file name from command line argument.
 	if(argv != NULL){
 		string saveFile{argv[1]};
@@ -77,22 +91,60 @@ int main(int argc, char** argv) {
 				if(str.size() > 0)
 					vect.push_back(str);
 			}		
-			// Close the file
-			inFile.close();
-		        
-			// Main Menu	
+		        inFile.close();
+			 
+			// Call main menu
+			vect = MainMenu(vect);
 
+			// Save back to file
+			std::ofstream outFile;
+			outFile.open(savePath);
+			for(int i=0; i<vect.size(); ++i){
+				outFile << vect[i] << std::endl;
+			}
+			outFile.close();
 		}
-		else
-			std::cout << "FILE NOT FOUND" << std::endl;
-	
 	}
-
-
-
 	return 0;
 }
 
+// Main Menu	
+std::vector<string> MainMenu(std::vector<string> vect){
+	int choice;
+	
+	// Main menu presented to user
+	std::cout << std::endl;
+	std::cout << "[1] Add Item" << std::endl;
+	std::cout << "[2] Delete Item" << std::endl;
+	std::cout << "[3] Enter List Item to Toggle Status" << std::endl;
+	std::cout << "[4] Sort List by Completeness" << std::endl;
+	std::cout << "[5] Exit" << std::endl;
+	std::cout << '\n' << "Enter Choice: ";
+	std::cin >> choice;
+	
+	// Switch for menu selection
+	switch(choice){
+		case 1:
+			std::cout << "Add Item" << std::endl;
+			break;
+		case 2:
+			std::cout << "Delete Item" << std::endl;
+			break;
+		case 3:
+			std::cout << "Toggle Status" << std::endl;
+			break;
+		case 4:
+			std::cout << "Sort by Completeness" << std::endl;
+			break;
+		case 5:
+			std::cout << "Exit" << std::endl;
+			break;
+		default: 
+			std::cout << "Invalid Input" << std::endl;
+	}
+	// Return the vector 
+	return vect;		
+}
 
 
 
