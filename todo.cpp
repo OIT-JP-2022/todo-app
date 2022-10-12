86fe50b35f80123bc378eb0aa18f45216bc9a9a1
Gabriel Browning <ghbscout@gmail.com>
2022-10-11 23:59:54 -0700

    Reformatted large portion of code to use vectors

--- todo.cpp
+++ todo.cpp
@@ -7,22 +7,15 @@
 #include <bits/stdc++.h>
 using listitem = std::pair<std::string,bool>;
 using listitems = std::vector<listitem>;
-using namespace std;
 int ValidateCall(int InputCount, char** argv);
-bool OptionsMenu(std::fstream& file);
-void Print(std::fstream& file);
-bool SortList(std::fstream& file);
-bool RemoveItem(std::fstream& file);
+bool OptionsMenu(listitems& items);
+void Print(listitems& items);
+bool SortList(listitems& items);
+bool RemoveItem(listitems& items);
+bool LastCharBool(std::string str);
+listitems FileListToVector(std::fstream& file);
+void FileListFromVector(listitems items, std::fstream& file);
 
-std::vector<std::string> MenuOptions
-{	
-  "1. Add new ToDo item",
-  "2. Mark ToDo items Complete/Incomplete",
-  "3. Remove ToDo item",
-  "4. Sort ToDo items",
-  "5. Save and Quit"
-
-};
 int main(int argc,char*argv[]) 
 {
     if (ValidateCall(argc,argv))
@@ -35,9 +28,37 @@ int main(int argc,char*argv[])
         return 1;
     }
 
-    while (OptionsMenu(ListFile));
+    listitems items(FileListToVector(ListFile));
+
+
+    while (OptionsMenu(items));
+    
+  //  FileListFromVector(items, ListFile);
+
     return 0;
 }
+
+
+listitems FileListToVector(std::fstream& file){
+    listitems items;
+    std::string line;
+    while (std::getline(file, line)){
+        bool check = LastCharBool(line);
+        line.pop_back();
+        listitem newitem(line,check);
+        items.push_back(newitem); 
+    }
+    return items;
+
+}
+
+void SaveFileListFromVector(listitems items, std::fstream& file){
+    file.clear();
+    file.seekg(0);
+    for (auto& item: items)
+        file << item.first << item.second << "\n";
+}
+
 
 int ValidateCall(int InputCount, char** argv)
 {
@@ -117,61 +138,58 @@ bool RemoveItem(std::fstream& file)
 //}
 
 
-
-bool AddNewItem(std::fstream& file)
+bool AddNewItem(listitems& items)
 {
-  Print(file);
+  Print(items);
   std::cout << "Enter your ToDo list item to add or '-q' to quit adding: \n =>";
-  std::string item;
-  std::getline(std::cin,item); 
-  if (item == "-q")
-	  return false;
-  file.clear();
-  file.seekg(0,std::ios::end);
-  file << item  << "F\n";
+  std::string option;
+  std::getline(std::cin, option);
+  if (option == "-q")
+    return false;
 
-  std::cout << "Added a new incomplete item: \n" << item << "\n";
+  for (auto& item: items)
+    if (item.first == option){
+        std::cout << "Item already in list";
+        return true;
+    }
+        
+  listitem newitem(option, false);
+
+  items.push_back(newitem);
+
+  std::cout << "Added a new incomplete item: \n" << option << "\n";
   return true;
 }
 
 
-bool UpdateItem(std::fstream& file){
-    Print(file);
+bool UpdateItem(listitems& items){
+    Print(items);
     std::cout << "What Item would you like to update or '-q' to quit editing: \n =>";
-
-    std::string item;
-    std::getline(std::cin,item); 
-    std::cout << item << "\n";
-    if (item == "-q")
+    std::string option;
+    std::getline(std::cin,option); 
+    if (option == "-q")
 	  return false;
-
-    std::string line;
-    file.clear();
-    file.seekg(0, std::ios::beg);
-    int pos = 0;
-    while(std::getline(file, line)){
-        bool complete = LastCharBool(line);
-        line.pop_back();
-        
-        
-        if (line == item){
-            (complete) ? line.append("F") : line.append("T");
-            file.seekg(pos,std::ios::beg);
-            file << line;
-            
-        }
-
-        pos = file.tellg();
-        
-    }
+    
+    for (auto& item: items)
+        if (item.first == option)
+            item.second = !item.second;
 
     return true;
 }
 
 
-bool OptionsMenu(std::fstream& file)
+bool OptionsMenu(listitems& items)
 {
  
+std::vector<std::string> MenuOptions
+{	
+  "1. Add new ToDo item",
+  "2. Mark ToDo items Complete/Incomplete",
+  "3. Remove ToDo item",
+  "4. Sort ToDo items",
+  "5. Save and Quit"
+
+};
  for(auto &item:MenuOptions)
    std::cout << item << "\n";
  std::cout << " =>";
@@ -180,19 +198,19 @@ bool OptionsMenu(std::fstream& file)
  std::cin.ignore();
  switch(option){
 	case 1:
-	 while (AddNewItem(file));
+	 while (AddNewItem(items));
 	 return true;
 	case 2: 
 	 //Mark Todo Items
-         while (UpdateItem(file));
+         while (UpdateItem(items));
          return true;
 	case 3: 
 	 //Remove Todo Items
-	 while(RemoveItem(file));
+//	 while(RemoveItem(items));
 	 //method call
 	 return true;
 	case 4: 
-	 while(SortList(file));
+	 //while(SortList(items));
 	 return true;
 	case 5: 
 	 //Save and Quit
@@ -202,27 +220,22 @@ std::cout << "You did not select a valid option!";
 return true;
  }
 }
-
+/*
 bool SortList(std::fstream& file){
 	//std::sort(
         //file.seekg(0, std::ios::beg);
 	//ListFile.begin(), ListFile.end();
         //};
-	cout << "List has been Sorted! \n";
+	std::cout << "List has been Sorted! \n";
 	Print(file);
 	return false;
-}
+}*/
 
-void Print(std::fstream& file){
+void Print(listitems& items){
     std::cout << " ===== List of all todo items ===== \n";
-    std::string item;
-    file.clear();
-    file.seekg(0, std::ios::beg);
-    while (std::getline(file, item)){
-        bool completed = LastCharBool(item);
-        std::string check= (completed) ? "✓" : "X";
-        item.pop_back();
-        std::cout << "[" << check << "]\t" << item << "\n";
+    for (auto& item: items) {
+        std::string check = (item.second) ? "✓" : "X";
+        std::cout << "[" << check << "]\t" << item.first << "\n";
     }
 }
 
