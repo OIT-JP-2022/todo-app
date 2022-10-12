45e77410b1a74d5bbda2dc06bbbe6d184dba27d5
chandlormartin <chandlor.martin@oit.edu>
2022-10-11 23:19:25 -0700

    Created a text fil that contains a todo list and created a .cpp that loads the file into a list before displaying said list.

--- todo.cpp
+++ todo.cpp
@@ -1,144 +1,143 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <list>
 #include <fstream>
-#include <string.h>
+#include <vector>
+
 using namespace std;
 using std::string;
 using std::cout;
 using std::endl;
 using std::ifstream;
- class toDo
+using std::vector;
+
+class toDo
 {
-public:
-toDo(){
+	public:
+	toDo()
+	{
+		this->completeness = 0;
+		this->description = "";
+	}
 
-	this->completeness = 0;
-	this->description = "";
- }
+	void addDescription(string y){description = y;}
+	void addCompleteness(string b){completeness = stoi(b);}
 
-//toDo::toDo(string c, string d){
-//this->completeness = stoi(c);
-//this->description = d;
-//}
+	void clear()
+	{
+		description = "";
+		completeness = 0;
+	}
 
-void addDescription(string y){
-	description = description + y;
-}
-void addCompleteness(string b){
-        completeness = stoi(b);
-}
+	void print()
+	{
+		cout <<"\n Printing TODO ITEM \n" ;
+		cout<<"Description: " << description << "\n Completeness: " << completeness<<"\n";
+		cout << "END OF TODO PRINT \n";
+	}
 
-void clear(){
-description = "";
-completeness = 0; //eeeeee
-}
-
-
-int completeness;
-string description;
+	int	completeness;
+	string	description;
 };
 
 
 
- class toDoList
+class toDoList
 {
-public:
-toDoList() {theList = {}; }
+	public:
+	toDoList() {theList = {};}
 
-~toDoList(){
-while(theList.size() != 0){
-//delete theList.front();
-theList.pop_front();
-}
+	void printList()
+	{
+		for(auto const& i: theList)			// = theList.cbegin();i != theList.cend(); ++i){
+		{
+			cout << "Description: " << i.description << "\n";
+			cout << "Completeness: " << i.completeness << "\n \n"; 
+		}
+	}
+	
+	void toDoAdder(toDo x) { theList.push_back(x);};	//This adds 'x' (toDo)  to our toDo list (theList)
 
-}
+	void loadList(string y)
+	{
+		string filename = y;
+		string words;
+		string word;
+		string desc = "";
+		string comp = "";
+		ifstream infile(filename);
+		
 
-void printList(){
-for(auto const& i: theList)// = theList.cbegin();i != theList.cend(); ++i){
-{
-cout << i.description << "\n";
-cout << i.completeness << "\n"; 
-}
-}
-void toDoAdder(toDo x) {
+		if(!infile.is_open())
+			cout << "cannot open file " <<  filename << "\n";
+	
+	
+		while(infile >> word)
+		{
+			if(word.compare("Start:") == 0 && words.length() != 0)
+			{
+				//This creates a toDo item and adds the current description and completeness level to it.
+				//It then places this item at the back of myList.
 
-theList.push_front(x); //This adds 'x' (toDo)  to our toDo list (theList)
+				comp = words;
+				toDo newItem;
+				
+				newItem.addDescription(desc);
+				newItem.addCompleteness(comp);
+				theList.push_back(newItem);
+				
+				desc = ""; 
+				comp = "";
+				words = "";
+			}
+			else if(word.compare("Start:") == 0 && words.length() == 0){}
+			else if(word.compare("Completeness:") == 0)
+			{
+				//This sets desc and clears words
+				desc = words;	
+				words=""; 
+			}
+			else
+			{
+				//This appends the word to words
+				words = words + (" " + word);
+			}
+		}
 
-	          }
-void loadList(string y){ //y is our file name variable
-string desc;
-string comp;
+		//adds the last toDo item
+		comp=words;
+		toDo newItem;
+                newItem.addDescription(desc);
+                newItem.addCompleteness(comp);
+		theList.push_back(newItem);
+	}
 
-toDo myToDo;
-
-cout << y << "\n";
-string filename = y;
-string words;
-string word;
-ifstream infile(filename);
-if(!infile.is_open()){
-	cout << "cannot open file " <<  filename << "\n";
-
-}
-toDo* item = new toDo();
-
-while(infile >> word){
-if(strcmp(word.c_str(), "Start:") == 0 && words.length() != 0){ //if word is start and nothing in list
-comp = words;
-//myToDo.addCompleteness(words); // Complete?
-toDo* item = new toDo();
-item->addDescription(desc);
-item->addCompleteness(comp);
-
-theList.emplace_back(item); //push to the bacc of myToDo
-desc = "";
-comp = "";
-//myToDo.clear(); //clear desc
-}
-else if(strcmp(word.c_str(), "Start:") == 0 && words.length() == 0){
-//myToDo.addCompleteness(words); //do nothing
-
-//toDo* item = new toDo();
-//item.addDescription(desc);
-item.addCompleteness(comp);
-
-theList.emplace_back(myToDo);
-
-}
-
-else if(strcmp(word.c_str(), "Completeness:") == 0){
-desc = words;
-//myToDo.addDescription(words); //adds missing words
-words.clear(); //clear current list of words
-}
-
-else{
-words + " " + word; //if normal word place at the back of list
-}
-}
-
-}
-std::list<toDo> theList = {};
-
-
+	std::vector<toDo> theList = {};
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+	string argument = argv[1];
+	cout << "You have entered " << argc << " arguments: " << "\n";
+	//for(int i = 0; i <  argc; ++i)
+	//	cout << argv[i] << "\n";
 
-cout << "You have entered " << argc << " arguments: " << "\n";
-for(int i = 0; i <  argc; ++i)
-	cout << argv[i] << "\n";
+	toDoList myList; //create instance of list
+	if(argument.compare( "-l") == 0)  //check if -l
+		myList.loadList(argv[2]);
+	else
+	{
+		cout<<"Usage: Loads or saves a todo list from a .txt document. EX: todo -l filename.txt \n";
+		return 0;
+	}
 
-toDoList myList; //create instance of list
-if(strcmp(argv[1],  "-l") == 0) { //check if -l
-	myList.loadList(argv[2]);
+	myList.printList();
+}
 
- }
-
-myList.printList();
+//myList.printList();
 
 // std::cout << "todo: make todo app THIS IS JASON!..." << '\n';
-}
+//}
 
 
