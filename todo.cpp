fae25d25b5193eeeb6c827173fc7f0d441d07120
noah <noah.etch@gmail.com>
2022-10-11 17:27:15 -0700

    Working menu and insert

--- todo.cpp
+++ todo.cpp
@@ -6,31 +6,71 @@ using std::cout;
 using std::cin;
 using std::string;
 
-std::fstream openFile(std::string filename){
+std::fstream openFile(const string& filename){
     std::fstream File;
-    File.open(filename);
+    File.open(filename, std::ios_base::app | std::ios_base::in | std::ios_base::out);
     if(!File){ //no file, make one
       std::ofstream File(filename); //no file, make one
     } 
     return File;
-
 } 
 
+char menu()
+{
+    char choice;
+    cout << "-----ToDo Actions-----" << "\n";
+    cout << "a: add items" << "\n";
+    cout << "q: quit" << "\n";
+    cout << "\n";
+    cout << "Enter option: ";
+    cin >> choice;
+    return std::tolower(choice);
+}
+
+void printList(const string& filename) 
+{
+    std::fstream myFile = openFile(filename);
+    string text;
+    cout << "-------ToDo List------" << "\n";
+    if (myFile.is_open())
+    {
+        while (getline(myFile, text))
+        {
+            cout << text << "\n";
+        }
+    }else{
+      cout << "File not open";
+    }
+}
+
+void enterItem(const string& filename)
+{
+    std::fstream myFile = openFile(filename);
+    if (myFile.is_open())
+    {
+      string item = "";
+      cout << "Enter item: ";
+      cin.ignore();
+      getline(cin, item);
+      myFile << "- [ ] " << item << "\n";
+    }else{
+      cout << "File not open";
+    } 
+}
+
+
 int main(int argc, char *argv[]) {
+  std::string filename = argc >= 2 ? argv[1] : "todo.txt"; // arg or defaul
 
-  try {
-    std::fstream todoFile = argc >= 2 ? openFile(argv[1]) : openFile("todo.txt");
-  }catch (const string& ex){
-    cout << ex;
-  }
-  
-  
+  bool exit = false;
+  do{
+    printList(filename);
+    switch(menu()){
+      case 'a': enterItem(filename); break;
+      case 'q': exit = true; break;
+    }
+  }while (!exit);
 
-  // if (todoFile.is_open()){
-  //   cout << "It worked\n";
-  // }else {
-  //   cout << "It didint worked\n";
-  // }
-  cout << "\n\n";
+  cout << "\nHave a good day!\n";
   return 0;
 }
