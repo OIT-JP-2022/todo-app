8d649fc98da5c2aac1fd2f2f4d143def9ca95ceb
noah <noah.etch@gmail.com>
2022-10-11 21:05:42 -0700

    Project 1 Complete

--- todo.cpp
+++ todo.cpp
@@ -2,74 +2,125 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 using std::cout;
 using std::cin;
 using std::string;
 
-std::fstream openFile(const string& filename){
-    std::fstream open_file;
-    open_file.open(filename, std::ios_base::app | std::ios_base::in | std::ios_base::out);
-    if(!open_file) std::ofstream File(filename); //no file, make one
-    if (open_file.fail()) throw "Open file failed."; // File doesn't open, error out
-    return open_file;
-} 
+std::vector<string> readToVector(const string& filename){
+  std::ifstream myFile; 
+  myFile.open(filename);
+  if(myFile.fail()) throw "Error opening file.";  
 
-char menu()
-{
-    char choice;
-    cout << "-----ToDo Actions-----" << "\n";
-    cout << "a: add items" << "\n";
-    cout << "q: quit" << "\n";
-    cout << "\n";
-    cout << "Enter option: ";
-    cin >> choice;
-    return std::tolower(choice);
+  std::vector<string> lines;
+  string line;
+  while (getline(myFile, line))
+  {
+    lines.push_back(line);
+  }
+  myFile.close();
+  return lines;
 }
 
-void printVector(const std::vector<string>& vector, const string& title = "-------ToDo List------\n") 
+void writeFromVector (const string& filename, const std::vector<string>& vector){
+  std::ofstream write_file;
+  write_file.open(filename);
+  if(write_file.fail()) throw "Error opening file.";  
+  
+  for (int i = 0; i < vector.size(); ++i){
+    write_file << vector[i] << "\n";
+  }
+  write_file.close();
+}
+
+void printVector(const std::vector<string>& vector, const string& title = "-------ToDo List------") 
 {
-  cout << title;
+  cout << "\n" << title << "\n";
   for (int i = 0; i < vector.size(); ++i){
     cout << (i+1) << ") " << vector[i] << "\n";
   }
 }
 
+void printVector(const string& filename, const string& title = "-------ToDo List------") 
+{
+  auto toPrint = readToVector(filename);
+  printVector(toPrint,title);
+}
+
 void enterItem(const string& filename)
 {
-  std::fstream myFile = openFile(filename);
+  auto data = readToVector(filename);
   string item = "";
   cout << "Enter item: ";
   cin.ignore();
   getline(cin, item);
-  myFile << "- [ ] " << item << "\n";
-  myFile.close();
+  data.push_back("- [ ] " + item + "\n");
+  writeFromVector(filename,data);
 }
 
-std::vector<string> getTasks(const string& filename){
-  std::fstream myFile = openFile(filename); 
-  std::vector<string> tasks;
-  string text;
-  while (getline(myFile, text))
-  {
-    tasks.push_back(text);
-  }
-  myFile.close();
+void deleteItem(const string& filename)
+{
+  auto tasks = readToVector(filename);
+  printVector(tasks,"----Edit ToDo List----");
+  int item = 0;
+  cout << "Which task would you like to delete: ";
+  cin >> item;
 
-  return tasks;
+  tasks.erase(tasks.begin()+(--item));
+  writeFromVector(filename, tasks);
+}
+
+void editItem(const string& filename)
+{
+  auto tasks = readToVector(filename);
+  printVector(tasks,"----Edit ToDo List----");
+  int item = 0;
+  cout << "Which task would you like to change: ";
+  cin >> item;
+  --item <= tasks.size() ? (tasks[item][3] = tasks[item][3] == 'X' ? ' ' : 'X') : throw "Select out of bounds.";
+  writeFromVector(filename, tasks);
+}
+
+void sortListByCompleted(const string& filename)
+{
+  auto data = readToVector(filename);
+  std::sort(data.begin(),data.end());
+  writeFromVector(filename,data);
+  printVector(data, "\n----- Sorted by completion-----\n");
+}
+
+void menu(const string& filename)
+{
+    char choice;
+    bool exit = false;
+    while(!exit){
+          printVector(filename);
+      cout << "-----ToDo Actions-----" << "\n";
+      cout << "a: add items" << "\n";
+      cout << "d: delete items" << "\n";
+      cout << "m: mark items" << "\n";
+      cout << "s: sort items" << "\n";
+      cout << "q: quit" << "\n";
+      cout << "\n";
+      cout << "Enter option: ";
+      cin >> choice;
+      switch(std::tolower(choice)){
+        case 'a': enterItem(filename); break;
+        case 'm': editItem(filename); break;
+        case 'd': deleteItem(filename); break;
+        case 's': sortListByCompleted(filename); break;
+        case 'q': exit = true; break;
+      }
+    }
+
 }
 
 int main(int argc, char *argv[]) {
   std::string filename = argc >= 2 ? argv[1] : "todo.txt"; // arg or defaul
 
-  bool exit = false;
-  do{
-    printVector(getTasks(filename));
-    switch(menu()){
-      case 'a': enterItem(filename); break;
-      case 'q': exit = true; break;
-    }
-  }while (!exit);
+  menu(filename);
 
   cout << "\nHave a good day!\n";
   return 0;
