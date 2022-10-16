a57576092cbd28657f2bc022f4256d700be37b60
Jason Wells <jfrostwells@gmail.com>
2022-10-15 17:57:33 -0700

    added some more functionality

--- todo.cpp
+++ todo.cpp
@@ -7,7 +7,6 @@ using Lines = std::vector<std::string>;
 auto addTodo(Lines & todos, const std::string & todo) {
   todos.push_back(todo);
 }
-
 auto removeTodo(Lines & todos, int index) {
   todos.erase(todos.begin()+index);
 }
@@ -46,7 +45,7 @@ auto writeTodos(const Lines & todos, const std::string & filename) {
 }
 
 auto prompt() {
-  std::cout << "What do you want to do? (add) " << '\n';
+  std::cout << "What do you want to do? (add |a|, remove |r|, print |p|) " << '\n';
   std::string line{};
   std::getline(std::cin, line);
   return line;
@@ -63,6 +62,29 @@ auto run(Lines & todos) {
     addTodo(todos, line);
     return true;
   } else if (option == "q" || option == "quit") {
+ 
+}   
+    //else if remove
+    else if(option == "r" || option == "remove"){
+    std::cout << "Remove todo: ";
+    std::string line{};
+    std::getline(std::cin, line);
+    int boi = stoi(line);
+    removeTodo(todos, boi);
+return true;
+}
+    //else if display/print
+
+else if(option == "p" || option == "print"){
+   // std::cout << " todo: ";
+   // std::string line{};
+    //std::getline(std::Cin, line);
+    print(todos);
+return true;
+
+    //else if search/#
+    //else if nest add
+
     return false;
   }
 
