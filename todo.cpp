d1fcf190c703d42c00b5494009c0cbba9779a6ed
Chris Keith <chris.keith@oit.edu>
2022-10-17 15:33:43 -0700

    updated vector structure to include children updated functions to comply with format troubleshooting read in bug

--- todo.cpp
+++ todo.cpp
@@ -2,51 +2,77 @@
 #include <fstream>
 #include <utility>
 #include <vector> 
-#include <string> 
+#include <string>
+#include <algorithm>
 
-using listitem = std::pair<std::string,bool>;
-using listitems = std::vector<listitem>;
+using todoItem = std::pair<std::string,bool>;
+using childVector = std::vector<todoItem>;
+using todoPair = std::pair<childVector, todoItem>; // if child is null, then no children
+using todoVector = std::vector<todoPair>;
+
+
 
 
 bool lastCharBool(std::string str){
     return str.back() == '0';
 }
 
+std::string getTodoPairTask(const todoPair& pair){
+	return pair.second.first;
+}
 
-listitems fileListToVector(char* fname){
+todoVector fileListToVector(char* fname){
     std::ifstream inFile{fname};
     if (!inFile.is_open())                                                              // inFile.fail() checks if a flag was set for file fname not existing 
-        std::cout << "\nFile \'" << fname << "\' will be created as a todolist\n";    // If file was not originally found, one will be made during saving. Inform the user
-    listitems items;
+        std::cout << "\nFile \'" << fname << "\' will be created as a todoVector\n";    // If file was not originally found, one will be made during saving. Inform the user
+    todoVector items;
     std::string line;
     while (std::getline(inFile, line)){
-        bool check = lastCharBool(line);
+		bool check = lastCharBool(line);
         line.pop_back();
-        listitem newitem(line,check);
-        items.push_back(newitem); 
+		if(line[0] == '\t'){
+			line.erase(0,1);
+			todoItem newitem(line,check);
+			items.back().first.push_back(newitem);
+		}
+		else{        
+			todoPair newitem(childVector{}, todoItem{line, check});
+			items.push_back(newitem);
+		}			
     }
     return items;
 }
 
 
-void saveToFileListFromVector(const listitems& items, char* fname){
+void saveToFileListFromVector(const todoVector& items, char* fname){
     std::remove(fname);
     std::ofstream outFile {fname};
-    for (auto& item: items)
-        outFile << item.first << item.second << "\n";
+    for (auto& todoPair: items){
+        todoItem parentInfo = todoPair.second;		
+		outFile << parentInfo.first << parentInfo.second << "\n";
+		for (auto& childitem: todoPair.first){		
+			outFile << '\t' << childitem.first << childitem.second << "\n";
+		}
+	}
 }
 
 
-void print(const listitems& items){
+void print(const todoVector& items){
     std::cout << "\n ===== List of all todo items ===== \n";
     if (items.empty()){
         std::cout << "\t** EMPTY **\n";
         return;
     }
-    for (auto& item: items) {
-        std::string check = (item.second) ? "✓" : "X";
-        std::cout << "[" << check << "]\t" << item.first << "\n";
-    }
+	for (auto& todoPair: items){
+        todoItem parentinfo = todoPair.second;				
+		std::string check = (parentinfo.second) ? "✓" : "X";
+        std::cout << "[" << check << "]\t" << parentinfo.first << "\n";
+
+		for (auto& childitem: todoPair.first){		
+			std::string check = (childitem.second) ? "✓" : "X";
+			std::cout << "\t[" << check << "]\t" << childitem.first << "\n";	
+		}
+	}
 }
 
 std::string getUserInput(){
@@ -57,58 +83,84 @@ std::string getUserInput(){
 }
 
 
-bool addNewItem(listitems& items){
+//  We need to continue editing functions below to account for the 
+//  changes make to todo, and the addition of children.  new for loops...
+
+bool addNewItem(todoVector& items){
     print(items);
     std::cout << "Enter your ToDo list item to add or '-q' to quit adding:\n";
     std::string option = getUserInput();
     if (option == "-q")
-        return false;
-
-    for (auto& item: items)
-        if (item.first == option){
-            std::cout << "Item already in list";
+        return false;    
+	
+	for (auto& item: items){
+        if (getTodoPairTask(item) == option){
+            std::cout << "Item already in list as parent";
             return true;
-        }
+        }		
+		for (auto& childitem: item.first){		
+			if (childitem.first == option){
+				std::cout << "Item already in list as child";
+				return true;
+			}				
+		}
+	}
 
-    listitem newitem(option, false);
+    todoPair newitem(childVector{}, todoItem{option, false});
     items.push_back(newitem);
-
-    std::cout << "Added a new incomplete item: \n" << option << "\n";
+    std::cout << "Added a new incomplete parent item: \n" << option << "\n";
     return true;
 }
 
 
-bool updateItem(listitems& items){
+bool updateItem(todoVector& items){
     print(items);
     std::cout << "What Item would you like to update or '-q' to quit editing:\n";
     std::string option = getUserInput();
     if (option == "-q")
 	  return false;
 
-    for (auto& item: items)
-        if (item.first == option)
-            item.second = !item.second; // For any occurences of the item, set it to the opposite of previous
-
+    for (auto& item: items){
+		
+        if (getTodoPairTask(item) == option){
+            item.second.second = !item.second.second; // For any occurences of the item, set it to the opposite of previous
+			for (auto& childitem: item.first){
+				childitem.second = item.second.second;
+			}
+		}
+		for (auto& childitem: item.first){
+			if (childitem.first == option){
+				childitem.second = !childitem.second;
+			}
+		}
+	}
     return true;
 }
 
 
-bool removeItem(listitems& items){
+bool removeItem(todoVector& items){
     print(items);
     std::cout << "Enter your ToDo list item to add or '-q' to quit adding:\n";
     std::string option = getUserInput();
     if (option == "-q")
         return false;
 
-    for (auto& item: items)
-        if (item.first == option)
-            items.erase(std::remove(items.begin(), items.end(), item), items.end());
-    
+    for (auto& item: items){
+        if (getTodoPairTask(item) == option){
+            items.erase(std::find(items.begin(), items.end(), item));  //std::remove(items.begin(), items.end(), item), items.end());
+			return true;
+		}
+		for(auto& childitem: item.first){
+			if(childitem.first == option){
+				item.first.erase(std::remove(item.first.begin(), item.first.end(), childitem), item.first.end());
+			}
+		}	
+	}
     return true;
 }
 
 
-bool optionsMenu(listitems& items){
+bool optionsMenu(todoVector& items){
     std::cout << "\n";
     std::vector<std::string> MenuOptions
     {
@@ -123,26 +175,26 @@ bool optionsMenu(listitems& items){
 
     std::string option = getUserInput();
     switch(option[0]){                      // TODO: Change methods of reading input here. Probably need a validate input function (function pointer vector?)
-	case '1':
-         while (addNewItem(items));
-	 return true;
-	case '2':
-	 //Mark Todo Items
-         while (updateItem(items));
-         return true;
-	case '3':
-	 //Remove Todo Items
-         while(removeItem(items));
-	 return true;
-	case '4':
-	 //while(sortItems(items));
-	 return true;
-	case '5':
-	 //Save and Quit
-	 return false;
-    default:
-         std::cout << "You did not select a valid option!";
-         return true;
+		case '1':
+			while (addNewItem(items));
+			return true;
+		case '2':
+			//Mark Todo Items
+			while (updateItem(items));
+			return true;
+		case '3':
+			//Remove Todo Items
+			while(removeItem(items));
+			return true;
+		case '4':
+			//while(sortItems(items));
+			return true;
+		case '5':
+			//Save and Quit
+			return false;
+		default:
+			std::cout << "You did not select a valid option!";
+			return true;
     }
 
 }
@@ -159,7 +211,7 @@ int main(int argc,char*argv[]){
         return 1;
     }
 
-    listitems items(fileListToVector(fname));
+    todoVector items(fileListToVector(fname));
     while (optionsMenu(items));
     saveToFileListFromVector(items, fname);
 
