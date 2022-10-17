ab063e29ba82f4390184fa49107d8fdfb1b96832
Moe O <dupeoguns@gmail.comO>
2022-10-17 13:07:02 -0700

    search function implemented without hashtag add subtodo item function implemented   user prompt not implemented yet. complete todo item function almost implemented.   able to complete item with a subitem   not able to individually complete subitem   user prompt not implemented yet.

--- todo.cpp
+++ todo.cpp
@@ -4,8 +4,8 @@
 
 using Lines = std::vector<std::string>;
 
-auto prompt(std::string action) {
-  std::cout << "What do you want to do " << action << "?\n";
+auto prompt() {
+  std::cout << "What do you want to do? ";
   std::string line{};
   std::getline(std::cin, line);
   return line;
@@ -15,9 +15,15 @@ auto addTodo(Lines & todos, const std::string & todo) {
   todos.push_back(todo);
 }
 
-auto addSubTodo(Lines & todos, const std::string & subtodo, int index) {
-	int newIndex = index + 1;
-	todos.emplace(todos.begin()+newIndex, subtodo);
+auto addSubTodo(Lines & todos,const std::string & subtodo, int index) {
+	if (todos[index-1].starts_with('X') == true) {
+			std::cout << "Item already completed. \n\n";
+			return;
+	}
+	else if (todos[index].starts_with(' ') == true)
+			index++;
+	std::string	new_subtodo = " --" + subtodo;
+	todos.emplace(todos.begin()+index, new_subtodo);
 }
 
 auto removeTodo(Lines & todos, int index) {
@@ -29,23 +35,30 @@ auto editTodo(Lines & todos, int index, const std::string & todo) {
 }
 
 auto searchTodo(const Lines & todos) {
-  std::string keyword =  prompt("search");
-  std::cout << "This is the keyword: " << keyword << '\n';
+  std::string keyword =  prompt();
+  std::cout << "\nTodo items found: \n";
   
   for(const auto todo : todos) {
     if (todo.find(keyword) != std::string::npos)
       std::cout << todo << '\n';
-    else
-      std::cout << "no match" << '\n';
+		}
+	std::cout << "===================\n\n";
+}
+
+auto completeTodo(Lines & todos,int index) {
+  std::string newstring = "X " + todos[index-1];
+  editTodo(todos,index-1,newstring);
+  while(todos[index].front() == ' ') {
+    removeTodo(todos,index);
   }
 }
 
-auto print(const std::string & todo) {
-  std::cout << todo << '\n';
+auto print(const std::string & todo) {;
+ std::cout  << todo << '\n';
 }
 
 auto print(Lines & todos) {
-  for (const auto & todo : todos) {
+	for (const auto & todo : todos) {
     print(todo);
   }
 }
@@ -69,21 +82,57 @@ auto writeTodos(const Lines & todos, const std::string & filename) {
   }
 }
 
-auto run(Lines & todos) {
-  print(todos);
-
-  auto option = prompt("add");
-  if (option == "a" || option == "add") {
-    std::cout << "Add todo: ";
+auto runAddOption(Lines & todos) {
+		std::cout << "Add todo: ";
     std::string line{};
     std::getline(std::cin, line);
     addTodo(todos, line);
-    return true;
-  } else if (option == "q" || option == "quit") {
-    return false;
-  }
+}
 
-  std::cout << '\n';
+auto run(Lines & todos) {
+  int menuSelection;
+	print(todos);
+  
+	std::cout << "===================\n";
+
+	std::cout << "Cabbage Todo List \n\n";
+	
+  std::cout << "1: add todo \n"
+						<< "2: add sub todo \n"
+						<< "3: remove todo \n"
+						<< "4: edit todo \n"
+						<< "5: search todo \n"
+						<< "6: complete todo\n"
+						<< "7: quit \n"
+						<< "\n";
+	std::cout << "Select a number: ";					
+	std::cin >> menuSelection;
+	std::cin.ignore();
+	switch(menuSelection) {
+		case 1:
+				runAddOption(todos);
+				break;
+		case 2:
+				addSubTodo(todos,"test subtodo", 1);
+				break;
+		case 3:
+				std::cout << "Not implemented yet.\n";
+				break;
+		case 4:
+				std::cout << "Not implemented yet.\n";
+				break;
+		case 5:
+				searchTodo(todos);
+				break;
+		case 6:
+				completeTodo(todos,3);
+				break;
+		case 7:
+				return false;
+				break;
+    default:
+				std::cout << "Invalid Selection. Try again \n";
+	}
 
   return true;
 }
@@ -91,11 +140,8 @@ auto run(Lines & todos) {
 int main() {
   std::string filename{"todos.txt"};
   auto todos = readTodos(filename);
-  print(todos);
- // while (run(todos)) {}
- // searchTodo(todos);
-  addSubTodo(todos, "\ttest sub-string!", 1);
-  print(todos);
+ // print(todos);
+	while (run(todos)) {}
   writeTodos(todos, filename);
 }
 
