91899ffb94583e6f406fee1aaa29505eccbf0195
FunkyTracy <a.funktracy@gmail.com>
2022-10-18 16:54:36 -0700

    nested lists working. Reads lists from directory. Need to format and take a directory as an argument

--- todo.cpp
+++ todo.cpp
@@ -11,23 +11,34 @@ auto getLists(const std::string & folderName) {
   std::string currentPath{std::filesystem::current_path()};
   currentPath += "/" + folderName;
   for (const auto & file : std::filesystem::directory_iterator(currentPath)) {
-	//lists.push_back(WHAT???);
-    std::cout << std::filesystem::path(file).filename() << "\n";
+    std::string tempName = std::filesystem::path(file).filename();
+    if(tempName.find(".txt") != -1) {
+      lists.push_back(tempName);
+    }
   }
-  
   return lists;
 }
 
 // Add new list
-auto addList(const std::string & input) {
-  std::ofstream newFile(input + ".txt");
+auto addList(Lines & lists, const std::string & input) {
+  std::ofstream newFile("testDir/" + input + ".txt");
   newFile.close();
+  lists.push_back(input + ".txt");
 }
 
 // Add list item
 auto addTodo(Lines & todos, const std::string & todo) {
   todos.push_back(todo);
 }
+
+// Delete file/list
+auto removeList(Lines & lists, int index) {
+  std::string file = lists[index];
+  lists.erase(lists.begin()+index);
+  std::filesystem::remove("testDir/" + file);
+}
+
+//mnt/c/Users/blued/source/CST316LinuxStuff/todo-app/
 
 // Delete todo item
 auto removeTodo(Lines & todos, int index) {
@@ -75,60 +86,6 @@ auto writeTodos(const Lines & todos, const std::string & folderName) {
   }
 }
 
-// Main list screen
-auto mainPrompt() {
-  std::cout << "\n\t==============";
-  std::cout << "\n\t  Todo Lists";
-  std::cout << "\n\t==============\n";
-  std::cout << "1) Add new list" << '\n';
-  std::cout << "2) Delete list" << '\n';
-  std::cout << "3) Edit list" << '\n';
-  std::cout << "4) Exit" << '\n';
-  std::cout << "\n\n\t~ Menu choice: ";
-  int choice{};
-  std::cin >> choice;
-  return choice;
-}
-
-// Process main menu item
-auto runMain(Lines & lists) {
-  ///////TODO function to print lists
-
-  auto option = mainPrompt();
-  switch(option) {
-	case 1: {
-	  std::cout << "\t~ New list name: ";
-	  std::string input{};
-	  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-	  std::getline(std::cin, input);
-	  addList(input);
-	  break;
-	  }
-	case 2: {
-	  int choice{};
-	  std::cout << "\t~ Enter item# to delete: ";
-	  std::cin >> choice;
-	  removeTodo(lists,choice - 1);
-	  break;
-	  }
-	case 3: {
-	  int choice{};
-	  std::string input{};
-	  std::cout << "\t~ Enter item# to edit: ";
-	  std::cin >> choice;
-	  std::cout << "\n\t~ Enter new item: ";
-	  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-	  std::getline(std::cin, input);
-	  //editList(lists,choice - 1,input);
-	  break;
-	  }
-
-	default: return false;
-  }
-
-  return true;
-}
-
 // Sub list screen menu
 auto listPrompt() {	
   std::cout << "\n\t-------------";
@@ -155,7 +112,7 @@ auto runList(Lines & todos) {
 	  std::string input{};
 	  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
 	  std::getline(std::cin, input);
-      addTodo(todos,input);
+          addTodo(todos,input);
 	  break;
 	  }
 	case 2: {
@@ -183,11 +140,65 @@ auto runList(Lines & todos) {
   return true;
 }
 
+// Main list screen
+auto mainPrompt(Lines & lists) {
+  std::cout << "\n\t==============";
+  std::cout << "\n\t  Todo Lists";
+  std::cout << "\n\t==============\n";
+  print(lists);
+  std::cout << "\n\t1) Add new list" << '\n';
+  std::cout << "\t2) Delete list" << '\n';
+  std::cout << "\t3) Edit list" << '\n';
+  std::cout << "\t4) Exit" << '\n';
+  std::cout << "\n\n\t~ Menu choice: ";
+  int choice{};
+  std::cin >> choice;
+  return choice;
+}
+
+// Process main menu item
+auto runMain(Lines & lists) {
+
+
+  auto option = mainPrompt(lists);
+  switch(option) {
+	case 1: {
+	  std::cout << "\t~ New list name: ";
+	  std::string input{};
+	  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+	  std::getline(std::cin, input);
+	  addList(lists, input);
+	  break;
+	  }
+	case 2: {
+	  //TODO: create remove list function
+	  int choice{};
+	  std::cout << "\t~ Enter list# to delete: ";
+	  std::cin >> choice;
+	  removeList(lists,choice - 1);
+	  break;
+	  }
+	case 3: {
+	  int choice{};
+	  std::cout << "\t~ Enter item# to edit: ";
+	  std::cin >> choice;
+	  std::string todoFile {lists[choice - 1]};
+	  auto todos = readTodos(todoFile);
+	  while(runList(todos)) {}
+	  break;
+	  }
+
+	default: return false;
+  }
+
+  return true;
+}
+
 int main() {
   std::string folderName{"testDir"};
   auto lists = getLists(folderName);
   
-  //print(lists);
+  print(lists);
 
-  //while (runMain(lists)) {}
+  while (runMain(lists)) {}
 }
