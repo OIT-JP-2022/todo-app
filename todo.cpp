30ff976832506559a43462385abef86eb9ee6d08
Chris Keith <chris.keith@oit.edu>
2022-10-17 17:26:44 -0700

    Added search added print by task added update function met requirements aside from folder option -will pass this to a subsequent sprint, theoretically...

--- todo.cpp
+++ todo.cpp
@@ -14,11 +14,25 @@ using todoVector = std::vector<todoPair>;
 
 
 bool lastCharBool(std::string str){
-    return str.back() == '0';
+	return str.back() == '1';
 }
 
 std::string getTodoPairTask(const todoPair& pair){
 	return pair.second.first;
+}
+
+void updateParents(todoVector& items){
+	for (auto& item: items){
+		bool childrenComplete = true;
+		for (auto& childitem: item.first){
+			if(!childitem.second){
+				childrenComplete = false;
+				item.second.second = false;
+			}			
+		}
+		if(item.first.size() != 0)
+			item.second.second = childrenComplete;
+	}
 }
 
 todoVector fileListToVector(char* fname){
@@ -40,9 +54,9 @@ todoVector fileListToVector(char* fname){
 			items.push_back(newitem);
 		}			
     }
+	updateParents(items);
     return items;
 }
-
 
 void saveToFileListFromVector(const todoVector& items, char* fname){
     std::remove(fname);
@@ -56,6 +70,15 @@ void saveToFileListFromVector(const todoVector& items, char* fname){
 	}
 }
 
+void printPairChildren(const todoPair& pairs){
+	todoItem parentinfo = pairs.second;
+	std::string check = (parentinfo.second) ? "✓" : "X";
+    std::cout << "[" << check << "]\t" << parentinfo.first << "\n";
+	for (auto& childitem: pairs.first){		
+		std::string check = (childitem.second) ? "✓" : "X";
+		std::cout << "\t[" << check << "]\t" << childitem.first << "\n";
+	}
+}
 
 void print(const todoVector& items){
     std::cout << "\n ===== List of all todo items ===== \n";
@@ -63,16 +86,37 @@ void print(const todoVector& items){
         std::cout << "\t** EMPTY **\n";
         return;
     }
-	for (auto& todoPair: items){
-        todoItem parentinfo = todoPair.second;				
-		std::string check = (parentinfo.second) ? "✓" : "X";
-        std::cout << "[" << check << "]\t" << parentinfo.first << "\n";
+	for (auto& pair: items){
+        printPairChildren(pair);
+	}
+	std::cout << "\n**At any time type # followed by text to search for an item\n\n";
+}
 
-		for (auto& childitem: todoPair.first){		
-			std::string check = (childitem.second) ? "✓" : "X";
-			std::cout << "\t[" << check << "]\t" << childitem.first << "\n";	
+bool searchPrint(const todoVector& items, std::string s){
+	s.erase(0,1);
+	std::cout << "\n\n\n\n\n\n\n\n";
+		std::cout << "Your search yielded the following results:\n";
+	for (const auto& item: items){
+		if (getTodoPairTask(item) == s){
+			printPairChildren(item);
+			return true;
+		}
+		for (const auto& childitem: item.first){
+			if (childitem.first == s){
+				printPairChildren(item);
+				return true;
+			}
 		}
 	}
+	return false;
+}
+
+bool isSearching(const todoVector& items, const std::string s){
+	if(s[0] == '#'){
+		searchPrint(items, s);
+		return true;
+	}
+	return false;
 }
 
 std::string getUserInput(){
@@ -82,22 +126,24 @@ std::string getUserInput(){
     return option;
 }
 
-
-//  We need to continue editing functions below to account for the 
-//  changes make to todo, and the addition of children.  new for loops...
-
 bool addNewItem(todoVector& items){
     print(items);
-    std::cout << "Enter your ToDo list item to add or '-q' to quit adding:\n";
-    std::string option = getUserInput();
-    if (option == "-q")
-        return false;    
-	
+	std::cout << "Would you like to add a (p)arent task or (c)hild task?\n";
+	std::cout << "... or enter '-q' to quit adding\n";
+	std::string type = getUserInput();
+	if (isSearching(items, type))
+		return true;
+	if (type == "-q")
+		return false;
+	std::cout << "Enter your ToDo list item to add:\n";
+	std::string option = getUserInput();  
+	if (isSearching(items, option))
+		return true;  	
 	for (auto& item: items){
-        if (getTodoPairTask(item) == option){
-            std::cout << "Item already in list as parent";
-            return true;
-        }		
+		if (getTodoPairTask(item) == option){
+			std::cout << "Item already in list as parent";
+			return true;
+		}		
 		for (auto& childitem: item.first){		
 			if (childitem.first == option){
 				std::cout << "Item already in list as child";
@@ -105,35 +151,50 @@ bool addNewItem(todoVector& items){
 			}				
 		}
 	}
-
-    todoPair newitem(childVector{}, todoItem{option, false});
-    items.push_back(newitem);
-    std::cout << "Added a new incomplete parent item: \n" << option << "\n";
+	if(type == "c"){
+		std::cout << "Enter the parent you wish to add this child to:\n";
+		std::string parent = getUserInput();
+		if (isSearching(items, parent))
+			return true;
+		for (auto& item: items){
+			if(getTodoPairTask(item) == parent){
+				item.first.push_back(todoItem{option, false});
+				std::cout << "Added a new incomplete child item: \n" << option << "\n";
+			}
+		}
+	}
+	else{
+		todoPair newitem(childVector{}, todoItem{option, false});
+		items.push_back(newitem);
+		std::cout << "Added a new incomplete parent item: \n" << option << "\n";
+	}  
+	updateParents(items);
     return true;
 }
-
 
 bool updateItem(todoVector& items){
     print(items);
     std::cout << "What Item would you like to update or '-q' to quit editing:\n";
     std::string option = getUserInput();
-    if (option == "-q")
+    if (isSearching(items, option))
+		return true;
+	if (option == "-q")
 	  return false;
 
     for (auto& item: items){
-		
         if (getTodoPairTask(item) == option){
             item.second.second = !item.second.second; // For any occurences of the item, set it to the opposite of previous
 			for (auto& childitem: item.first){
 				childitem.second = item.second.second;
 			}
 		}
-		for (auto& childitem: item.first){
+		for(auto& childitem: item.first){
 			if (childitem.first == option){
 				childitem.second = !childitem.second;
 			}
 		}
 	}
+	updateParents(items);
     return true;
 }
 
@@ -142,7 +203,9 @@ bool removeItem(todoVector& items){
     print(items);
     std::cout << "Enter your ToDo list item to add or '-q' to quit adding:\n";
     std::string option = getUserInput();
-    if (option == "-q")
+    if (isSearching(items, option))
+		return true;
+	if (option == "-q")
         return false;
 
     for (auto& item: items){
@@ -156,7 +219,18 @@ bool removeItem(todoVector& items){
 			}
 		}	
 	}
+	updateParents(items);
     return true;
+}
+
+void sortItems(todoVector& items) {
+	sort(items.begin(), items.end(),
+		[](const auto& x, const auto& y) { return x.second.second < y.second.second; });
+	for (auto& parent: items){
+		sort(parent.first.begin(), parent.first.end(),
+		[](const auto& x, const auto& y) {return x.second < y.second; });
+	}
+	print(items);
 }
 
 
@@ -168,13 +242,17 @@ bool optionsMenu(todoVector& items){
         "2. Mark ToDo items Complete/Incomplete",
         "3. Remove ToDo item",
         "4. Sort ToDo items",
-        "5. Save and Quit"
+        "5. Save and Quit",
+		"\n**At any time type # followed by text to search for an item"
     };
     for(auto &item: MenuOptions)
         std::cout << item << "\n";
 
     std::string option = getUserInput();
     switch(option[0]){                      // TODO: Change methods of reading input here. Probably need a validate input function (function pointer vector?)
+		case '#':
+			searchPrint(items, option);
+			return true;
 		case '1':
 			while (addNewItem(items));
 			return true;
@@ -188,6 +266,7 @@ bool optionsMenu(todoVector& items){
 			return true;
 		case '4':
 			//while(sortItems(items));
+			sortItems(items);
 			return true;
 		case '5':
 			//Save and Quit
@@ -202,7 +281,6 @@ bool optionsMenu(todoVector& items){
 int main(int argc,char*argv[]){
 
     char* fname{argv[1]};
-
 
     if (argc != 2)  // Check number of arguments (Program name, ToDo list file name)
     {
